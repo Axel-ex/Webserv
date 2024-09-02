134830498bc44207b11a5142bf942edd08d14f73
ebmarque <ebenezermarquezine@gmail.com>
2024-09-02 20:14:50 +0100

    Makefile created and c++98 compliance fixed

--- sources/signal.cpp
+++ sources/signal.cpp
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   signal.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Axel <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: ebmarque <ebmarque@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 10:07:02 by Axel              #+#    #+#             */
-/*   Updated: 2024/07/16 14:15:39 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:08:47 by ebmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <atomic>
+// #include <atomic>
 #include <csignal>
 
-std::atomic<bool> stopFlag(false);
+// std::atomic<bool> stopFlag(false);
+bool stopFlag = false;
 
 void sigHandler(int signum)
 {
